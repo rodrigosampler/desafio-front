dbe1014b4578fc8d6016dcd75d0f0960f28e2aec
rodrigosampler <rodrigoufcquixada@gmail.com>
2018-08-13 14:01:21 -0300

    base

---
+++ src/app/categorias/categorias.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriasComponent } from './categorias.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CategoriasComponent]
+})
+export class CategoriasModule { }
